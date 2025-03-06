bdddabd3ea6858e53e2c63e60efcf45b98668f57
NotTheNull <objnotnull@live.com>
2025-03-06 14:36:50 -0700

    Setting up the barebones of the python source

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    node = TextNode("Some text", TextType.URL, "https://boot.dev")
+    print(node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "Normal"
+    BOLD = "Bold"
+    ITALIC = "Italic"
+    CODE = "Codeblock"
+    URL = "Link"
+    IMAGE = "Image"
+
+
+class TextNode():
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, test):
+        if self.text_type != test.text_type: return False
+        if self.text != test.text: return False
+        return self.url == self.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
